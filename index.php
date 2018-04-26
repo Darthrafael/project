bbabc9b2f38902be1187451b76e6c76be6b5dd78
Rafael Monasso <rafael@monasso.net>
2018-04-26 13:17:08 +0200

    INITIAL COMMIT

---
+++ index.php
@@ -0,0 +1,82 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bookshelf</title>
+    <link href="HubStyle.css" rel="stylesheet" type="text/css">
+    <link rel="shortcut icon" href="favicon.ico"/>
+    <link rel="icon" href="favicon.ico"/>
+</head>
+<body>
+<div id="header">
+    <h1>The Fairy's Bookshelf!</h1>
+</div>
+<div id="main">
+    <div id="left">
+        <h2>My Stuff</h2>
+        <menu>
+            <li><a href="maidGenerator.php" target="_self">Maid Creator</a></li>
+            <li><a href="Multipurpose.php" target="_self">Coin Converter & Chant Genericificator</a></li>
+            <li><a href="GateCreation.php" target="_self">Gate Creation & Calculation</a></li>
+            <li><a href="townCreation.php" target="_self">Town Generation</a></li>
+            <li><a href="diceRoller.php" target="_self">Dice-Rolling</a></li>
+            <li><a href="heroDnD.php" target="_self">Heroscape DnD</a></li>
+            <li><a href="Things.php" target="_self">Random Colours</a></li>
+            <li><a href="characterGen.php" target="_self">Character Generator</a></li>
+            <li><a href="colourConverter.php" target="_self">Colour Converter</a></li>
+        </menu>
+        <hr>
+        <h2>Things that are Partially Mine:</h2>
+        <menu>
+            <li><a href="Weather.html" target="_self">Weather</a></li>
+            <li><a href="d100Chooser.php" target="_self">Random Things</a></li>
+        </menu>
+    </div>
+    <div id="right">
+        <h2>Things that aren't mine:</h2>
+        <h3>Random Generators:</h3>
+        <menu>
+            <li><a href="https://donjon.bin.sh" target="_blank">Donjon</a></li>
+            <li><a href="https://www.samcodes.co.uk/project/markov-namegen/" target="_blank">Markov Name Generator</a></li>
+            <li><a href="http://ephemer.kapsi.fi/FhtagnGenerator.php" target="_blank">Random Chant Generator</a></li>
+            <li><a href="http://www.mithrilandmages.com/" target="_blank">Mythril & Mages</a></li>
+            <li><a href="http://www.random-generator.com/index.php?title=Main_Page" target="_blank">Abulafia Random Generators</a></li>
+            <li><a href="http://www.seventhsanctum.com/index.php" target="_blank">Seventh Sanctum Random Generators</a></li>
+            <li><a href="http://chaoticshiny.com/" target="_blank">Chaotic Shiny Random Generators</a></li>
+        </menu>
+        <h3>Map Things:</h3>
+        <menu>
+            <li><a href="http://www-cs-students.stanford.edu/~amitp/game-programming/polygon-map-generation/demo.html" target="_blank">Island Generator</a></li>
+            <li><a href="https://www.redblobgames.com/maps/mapgen2/" target="_blank">Island Generator 2.0</a></li>
+            <li><a href="http://gmworldmap.com" target="_blank">Giant World Map</a></li>
+        </menu>
+        <hr>
+        <h3>Other:</h3>
+        <menu>
+            <li><a href="http://www.bureauofcommunication.com/" target="_blank">Bureau of Communication</a></li>
+            <li><a href="http://www.innawoods.net/" target="_blank">Innawoods</a></li>
+            <li><a href="http://opengeneral.sourceforge.net/db/icons/" target="_blank">Open Panzer Icons</a></li>
+            <li><a href="https://larzm42.github.io/dom4inspector/" target="_blank">Everything Dominions 4</a></li>
+            <li><a href="http://game-icons.net/" target="_blank">Fancy Icons</a></li>
+            <li><a href="https://www.spriters-resource.com/" target="_blank">Spriters' Resource</a></li>
+            <li><a href="https://www.dafont.com/" target="_blank">Fonts</a></li>
+            <li><a href="https://www.omnicalculator.com/math/percentage-change" target="_blank">% Change Calculator</a></li>
+            <li><a href="http://www.isitdownrightnow.com/" target="_blank">Is it down right now?</a></li>
+            <li><a href="https://www.dndbeyond.com/spells" target="_blank">D&D Spells</a></li>
+            <li><a href="https://www.draw.io/" target="_blank">Flowchart Creator</a></li>
+            <li><a href="http://www.enworld.org/forum/content.php?3166-Do-You-Know-Your-Glaive-Guisarme-From-Your-Bohemian-Earspoon" target="_blank">Fancy Weapons</a></li>
+            <li><a href="http://sporkforge.com/text/word_count.php" target="_blank">Word Counter</a></li>
+        </menu>
+    </div>
+    <div id="rightleft">
+        <h1>Lorem Ipsum or Something</h1>
+    </div>
+    <div id="rightright">
+        <h1>Lorem Ipsum or Something Else</h1>
+    </div>
+</div>
+<div id="footer">
+
+    Copyright <?php echo date("Y", time()); ?> Fairyfloss
+</div>
+</body>
+</html>
